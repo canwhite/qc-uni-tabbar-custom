b877db51bdc90b1c83e16eeba775fa2285fe016f
qiaochao <752716886@qq.com>
2021-04-07 09:29:38 +0800

    navbar

--- pages/common-pages/index.vue
+++ pages/common-pages/index.vue
@@ -5,15 +5,19 @@
 		<!--这些componets是通过css引入colorUI的样式，这里只是展示
 			自己选择好样式之后css引入就可以了
 		-->
-		<!--:isBack="true" -->
-		<cu-custom bgColor="bg-gradual-blue" >
-<!-- 			<block slot="backText">返回</block> -->
+
+<!-- 		<cu-custom bgColor="bg-gradual-blue" :isBack="true">
+			<block slot="backText">返回</block> 
 			<block slot="content">首页</block>
-<!-- 			<block slot="right">
+			<block slot="right">
 				<view class="action">
 					<view class="cu-load load-cuIcon" :class="!isLoad?'loading':'over'"></view>
 				</view> 
-			</block> -->
+			</block> 
+		</cu-custom> -->
+		
+		<cu-custom bgImage="https://image.weilanwl.com/color2.0/plugin/sylb2244.jpg" >
+			<block slot="content">首页</block>
 		</cu-custom>
 		
 		<view> 
